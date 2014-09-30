0b43feca9fd1b7e977192903276f37b2e6c9302b
Bochun Bai <bbc@sinofool.net>
2014-09-30 13:33:36 -0700

    move the *.a file to lib directory

--- build_libcurl_dist.sh
+++ build_libcurl_dist.sh
@@ -22,13 +22,14 @@ build_for_arch arm64 arm-apple-darwin /Applications/Xcode.app/Contents/Developer
 build_for_arch armv7s armv7s-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS8.0.sdk ${TMP_DIR}/armv7s || exit 4
 build_for_arch armv7 armv7-apple-darwin /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS8.0.sdk ${TMP_DIR}/armv7 || exit 5
 
+mkdir -p ${TMP_DIR}/lib/
 ${DEVROOT}/usr/bin/lipo \
 	-arch i386 ${TMP_DIR}/i386/lib/libcurl.a \
 	-arch x86_64 ${TMP_DIR}/x86_64/lib/libcurl.a \
 	-arch armv7 ${TMP_DIR}/armv7/lib/libcurl.a \
 	-arch armv7s ${TMP_DIR}/armv7s/lib/libcurl.a \
 	-arch arm64 ${TMP_DIR}/arm64/lib/libcurl.a \
-	-output ${TMP_DIR}/libcurl.a -create
+	-output ${TMP_DIR}/lib/libcurl.a -create
 
 cp -r ${TMP_DIR}/armv7s/include ${TMP_DIR}/
 curl -O https://raw.githubusercontent.com/sinofool/build-libcurl-ios/master/patch-include.patch
@@ -37,5 +38,5 @@ patch ${TMP_DIR}/include/curl/curlbuild.h < patch-include.patch
 DIST_DIR=${HOME}/Desktop/libcurl-ios-dist
 rm -rf ${DIST_DIR}
 mkdir ${DIST_DIR}
-cp -r ${TMP_DIR}/include ${TMP_DIR}/libcurl.a ${DIST_DIR}
+cp -r ${TMP_DIR}/include ${TMP_DIR}/lib ${DIST_DIR}
 
